7e27e3003f33b58555ad56b57ca2b281838481d1
Crucible Deploy <chris@dod.net>
2018-10-17 17:27:32 -0400

    CRUCIBLE-7: prevent unit tests from running on production deploys

--- contracts/Crucible.sol
+++ contracts/Crucible.sol
@@ -6,7 +6,7 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Crucible is Ownable {
   using SafeMath for uint256;
 
-  bytes8 public version = "1.0.4";
+  bytes8 public version = "1.0.5";
 
   address public beneficiary;
   bool public calculateFee = false;
