1d06a0a29ab6df388bd1ee478e416e8df4d4ca5a
Crucible Deploy <chris@dod.net>
2018-10-26 11:29:51 -0400

    CRUCIBLE-10: rewrite truffle.js to be more clean

--- truffle.js
+++ truffle.js
@@ -1,32 +1,26 @@
 const child_process = require('child_process');
-const bip39 = require('bip39');
-const hdkey = require('ethereumjs-wallet/hdkey');
-const ProviderEngine = require('web3-provider-engine');
-const WalletSubprovider = require('web3-provider-engine/subproviders/wallet');
-const NonceSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
-const Web3Subprovider = require('web3-provider-engine/subproviders/web3');
 const Web3 = require('web3');
-
-const FilterSubprovider =
-  require('web3-provider-engine/subproviders/filters.js');
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 var web3 = new Web3();
 var environment = process.env.CRUCIBLE_ENV || 'development';
-var providerUrl;
-
-if (environment === 'production') {
-  providerUrl = 'https://mainnet.infura.io/';
-} else if (environment === 'staging') {
-  providerUrl = 'https://rinkeby.infura.io/';
-} else if (environment === 'ropsten') {
-  providerUrl = 'https://ropsten.infura.io/';
-} else if (environment === 'kovan') {
-  providerUrl = 'https://kovan.infura.io/';
-} else {
-  providerUrl = 'https://rinkeby.infura.io/';
-}
+var provider;
 
 if (environment !== 'development') {
+  var providerUrl;
+
+  if (environment === 'production') {
+    providerUrl = 'https://mainnet.infura.io/';
+  } else if (environment === 'staging') {
+    providerUrl = 'https://rinkeby.infura.io/';
+  } else if (environment === 'ropsten') {
+    providerUrl = 'https://ropsten.infura.io/';
+  } else if (environment === 'kovan') {
+    providerUrl = 'https://kovan.infura.io/';
+  } else {
+    providerUrl = 'https://rinkeby.infura.io/';
+  }
+
   // unseal the vault
   child_process.execSync('./scripts/vault_unseal.js');
 
@@ -34,29 +28,8 @@ if (environment !== 'development') {
   var mnemonic = child_process.execSync(
     './scripts/vault_get_mnemonic.js'
   ).toString().replace(/\n/, '');
-  var hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
 
-  // Get the first account using the standard hd path.
-  var wallet_hdpath = "m/44'/60'/0'/0/";
-  var wallet = hdwallet.derivePath(wallet_hdpath + '0').getWallet();
-  var address = '0x' + wallet.getAddress().toString('hex');
-
-  // build the provider engine
-  var engine = new ProviderEngine();
-
-  // engine filters
-  engine.addProvider(new FilterSubprovider());
-  engine.addProvider(new WalletSubprovider(wallet, {}));
-  engine.addProvider(
-    new Web3Subprovider(new Web3.providers.HttpProvider(providerUrl))
-  );
-
-  // try to fix nonce too low errors
-  var nonceSubprovider = new NonceSubprovider();
-  engine._providers.unshift(nonceSubprovider);
-  nonceSubprovider.setEngine(engine);
-
-  engine.start();
+  provider = new HDWalletProvider(mnemonic, providerUrl);
 }
 
 module.exports = {
@@ -67,26 +40,22 @@ module.exports = {
       network_id: "*"   // Match any network id
     },
     staging: {
-      provider: engine,
-      from: address,
+      provider: provider,
       gasPrice: web3.toWei('100', 'gwei'),
       network_id: '4',  // Official rinkeby network id
     },
     production: {
-      provider: engine,
-      from: address,
+      provider: provider,
       gasPrice: web3.toWei('10', 'gwei'),
       network_id: "1",  // Main Ethereum Network
     },
     ropsten: {
-      provider: engine,
-      from: address,
+      provider: provider,
       gasPrice: web3.toWei('10', 'gwei'),
       network_id: '3',  // Official ropsten network id
     },
     kovan: {
-      provider: engine,
-      from: address,
+      provider: provider,
       gasPrice: web3.toWei('10', 'gwei'),
       network_id: '42',  // Official kovan network id
     },
